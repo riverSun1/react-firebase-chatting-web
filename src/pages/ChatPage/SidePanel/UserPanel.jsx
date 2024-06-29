f2f036382e43788adb3bb9e5a29fbf26cdf3a7b3
riverSun1 <haeweon0426@gmail.com>
2024-06-29 12:23:09 +0900

    feat: 프로필 수정 기능 완료

--- src/pages/ChatPage/SidePanel/UserPanel.jsx
+++ src/pages/ChatPage/SidePanel/UserPanel.jsx
@@ -1,15 +1,24 @@
+import { getAuth, signOut, updateProfile } from "firebase/auth";
+import { ref as dbRef, update } from "firebase/database";
+import {
+  getDownloadURL,
+  getStorage,
+  ref as strRef,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { useRef } from "react";
 import { Image } from "react-bootstrap";
-import { IoIosChatboxes } from "react-icons/io";
-import { useSelector } from "react-redux";
 import Dropdown from "react-bootstrap/Dropdown";
-import firebase from "../../../firebase";
-import { getAuth, signOut } from "firebase/auth";
+import { IoIosChatboxes } from "react-icons/io";
+import { useDispatch, useSelector } from "react-redux";
+import firebase, { db } from "../../../firebase";
+import { setPhotoUrl } from "../../../redux/slices/userSlice";
 
 const UserPanel = () => {
+  const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
-  // console.log(currentUser);
-
   const auth = getAuth(firebase);
+  const inputOpenImageRef = useRef(null);
 
   const handleLogout = () => {
     signOut(auth)
@@ -17,6 +26,72 @@ const UserPanel = () => {
       .catch((err) => {
         console.error(err);
       });
+  };
+
+  const handleOpenImageRef = () => {
+    inputOpenImageRef.current.click();
+  };
+
+  const handleUploadImage = (event) => {
+    const file = event.target.files[0];
+    const user = auth.currentUser;
+
+    const storage = getStorage(firebase);
+    const metadata = {
+      contentType: file.type,
+    };
+    const storageRef = strRef(storage, "user_image/" + user.uid);
+    const uploadTask = uploadBytesResumable(storageRef, file, metadata);
+
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+        }
+      },
+      (error) => {
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+          case "storage/unauthorized":
+            // User doesn't have permission to access the object
+            break;
+          case "storage/canceled":
+            // User canceled the upload
+            break;
+
+          // ...
+
+          case "storage/unknown":
+            // Unknown error occurred, inspect error.serverResponse
+            break;
+        }
+      },
+      () => {
+        // Upload completed successfully, now we can get the download URL
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log("File available at", downloadURL);
+
+          updateProfile(user, {
+            photoURL: downloadURL,
+          });
+
+          dispatch(setPhotoUrl(downloadURL));
+          update(dbRef(db, `users/${user.uid}`), { image: downloadURL });
+        });
+      }
+    );
   };
 
   return (
@@ -35,11 +110,20 @@ const UserPanel = () => {
             {currentUser.displayName}
           </Dropdown.Toggle>
           <Dropdown.Menu>
-            <Dropdown.Item>프로필 사진</Dropdown.Item>
+            <Dropdown.Item onClick={handleOpenImageRef}>
+              프로필 사진 변경
+            </Dropdown.Item>
             <Dropdown.Item onClick={handleLogout}>로그아웃</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
       </div>
+      <input
+        onChange={handleUploadImage}
+        type="file"
+        ref={inputOpenImageRef}
+        className="hidden"
+        accept="image/jpeg, image/png"
+      />
     </div>
   );
 };
