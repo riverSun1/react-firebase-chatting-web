33cf7a5fcbefa8e7d3f9bf51694327aace9f9e85
riverSun1 <haeweon0426@gmail.com>
2024-06-29 11:18:30 +0900

    feat: 로그아웃 기능 구현

--- src/pages/ChatPage/SidePanel/UserPanel.jsx
+++ src/pages/ChatPage/SidePanel/UserPanel.jsx
@@ -1,21 +1,44 @@
 import { Image } from "react-bootstrap";
 import { IoIosChatboxes } from "react-icons/io";
 import { useSelector } from "react-redux";
+import Dropdown from "react-bootstrap/Dropdown";
+import firebase from "../../../firebase";
+import { getAuth, signOut } from "firebase/auth";
 
 const UserPanel = () => {
   const { currentUser } = useSelector((state) => state.user);
-  console.log(currentUser);
+  // console.log(currentUser);
+
+  const auth = getAuth(firebase);
+
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {})
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
   return (
     <div>
-      <h3 className="text-white">
-        <IoIosChatboxes />
+      <h3 className="flex flex-row text-2xl font-bold text-white gap-2">
+        <IoIosChatboxes /> Chat APP
       </h3>
-      <div>
+      <div className="flex flex-row">
         <Image
           src={currentUser.photoURL}
           roundedCircle
           className="w-8 h-8 mt-1"
         />
+        <Dropdown>
+          <Dropdown.Toggle className="bg-transparent border-0">
+            {currentUser.displayName}
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item>프로필 사진</Dropdown.Item>
+            <Dropdown.Item onClick={handleLogout}>로그아웃</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
       </div>
     </div>
   );
