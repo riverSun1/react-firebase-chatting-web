b89e4242e6521d96723475432cc52c3eb275870b
riverSun1 <haeweon0426@gmail.com>
2024-07-02 12:53:40 +0900

    feat: 공개채팅방 즐겨찾기 기능 구현 완료

--- src/pages/ChatPage/MainPanel/MessageHeader.jsx
+++ src/pages/ChatPage/MainPanel/MessageHeader.jsx
@@ -1,11 +1,57 @@
+import { child, onValue, ref, remove, update } from "firebase/database";
+import { useEffect, useState } from "react";
 import { Col, FormControl, Image, InputGroup, Row } from "react-bootstrap";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaLock, FaLockOpen } from "react-icons/fa";
+import { MdFavorite, MdFavoriteBorder } from "react-icons/md";
 import { useSelector } from "react-redux";
+import { db } from "../../../firebase";
 
 const MessageHeader = ({ handleSearchChange }) => {
   const { currentChatRoom } = useSelector((state) => state.chatRoom);
   const { isPrivateChatRoom } = useSelector((state) => state.chatRoom);
+  const { currentUser } = useSelector((state) => state.user);
+  const [isFavorite, setIsFavorite] = useState(false);
+  const usersRef = ref(db, "users");
+
+  useEffect(() => {
+    if (currentChatRoom.id && currentUser.uid) {
+      addFavoriteListener(currentChatRoom.id, currentUser.uid);
+    }
+  }, [currentChatRoom.id, currentUser.uid]);
+
+  // Favorite 리스너 추가
+  // 데이터를 받아와 현재 채팅방이 즐겨찾기에 추가되어 있는지 확인 후 isFavorite 상태 업데이트
+  const addFavoriteListener = (chatRoomId, userId) => {
+    onValue(child(usersRef, `${userId}/favorite`), (data) => {
+      if (data.val() !== null) {
+        const chatRoomIds = Object.keys(data.val());
+        const isAlreadyFavorite = chatRoomIds.includes(chatRoomId);
+        setIsFavorite(isAlreadyFavorite);
+      }
+    });
+  };
+
+  const handleFavorite = () => {
+    if (isFavorite) {
+      setIsFavorite(false);
+      remove(
+        child(usersRef, `${currentUser.uid}/favorite/${currentChatRoom.id}`)
+      );
+    } else {
+      setIsFavorite(true);
+      update(child(usersRef, `${currentUser.uid}/favorite`), {
+        [currentChatRoom.id]: {
+          name: currentChatRoom.name,
+          description: currentChatRoom.description,
+          createdBy: {
+            name: currentChatRoom.createdBy.name,
+            image: currentChatRoom.createdBy.image,
+          },
+        },
+      });
+    }
+  };
 
   return (
     <div className="w-full border-2 rounded-md h-50 p-1 mb-1">
@@ -24,6 +70,11 @@ const MessageHeader = ({ handleSearchChange }) => {
         <Col className="flex flex-row gap-2 items-center">
           <h2>{isPrivateChatRoom ? <FaLock /> : <FaLockOpen />}</h2>
           <p>{currentChatRoom.name}</p>
+          {isPrivateChatRoom ? null : (
+            <p className="cursor-pointer" onClick={handleFavorite}>
+              {isFavorite ? <MdFavorite /> : <MdFavoriteBorder />}
+            </p>
+          )}
         </Col>
         <Col>
           <InputGroup>
--- src/pages/ChatPage/SidePanel/Favorite.jsx
+++ src/pages/ChatPage/SidePanel/Favorite.jsx
@@ -1,5 +1,65 @@
+import {
+  child,
+  off,
+  onChildAdded,
+  onChildRemoved,
+  ref,
+} from "firebase/database";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { db } from "../../../firebase";
+
 const Favorite = () => {
-  return <div>⭐ 즐겨찾기</div>;
+  const [favoriteChatRooms, setFavoriteChatRooms] = useState([]);
+  const usersRef = ref(db, "users");
+
+  const { currentUser } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (currentUser.uid) {
+      addListener(currentUser.uid);
+    }
+    return () => {
+      removeListener(currentUser.uid);
+    };
+  }, [currentUser.uid]);
+
+  const removeListener = (userId) => {
+    off(child(usersRef, `${userId}/favorite`));
+  };
+
+  const addListener = (userId) => {
+    let favoriteArray = [];
+    onChildAdded(child(usersRef, `${userId}/favorite`), (DataSnapshot) => {
+      favoriteArray.push({ id: DataSnapshot.key, ...DataSnapshot.val() });
+      const newFavoriteChatRooms = [...favoriteArray];
+      setFavoriteChatRooms(newFavoriteChatRooms);
+    });
+
+    onChildRemoved(child(usersRef, `${userId}/favorite`), (DataSnapshot) => {
+      const filteredChatRooms = favoriteArray.filter((chatRoom) => {
+        return chatRoom.id !== DataSnapshot.key;
+      });
+      favoriteArray = filteredChatRooms;
+      setFavoriteChatRooms(filteredChatRooms);
+    });
+  };
+
+  const renderFavoriteChatRooms = (favoriteChatRooms) => {
+    return (
+      favoriteChatRooms.length > 0 &&
+      favoriteChatRooms.map((chatRoom) => (
+        <li key={chatRoom.id}>- {chatRoom.name}</li>
+      ))
+    );
+  };
+
+  return (
+    <div>
+      <div>⭐ 즐겨찾기</div>
+      <ul>{renderFavoriteChatRooms(favoriteChatRooms)}</ul>
+    </div>
+  );
 };
 
 export default Favorite;
